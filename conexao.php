60145b6c1878dffcf9f66a170c91c325ef1fcf58
Joao Paulo <joao_paulo_oandrade@hotmail.com>
2025-05-07 20:15:49 -0300

    adicionando a string de conexao

--- conexao.php
+++ conexao.php
@@ -1,17 +1,23 @@
 <?php
 
+$host = "localhost";
+$usuario= "root";
+$senha= "12345678";
+$banco= "BDPHP";
 
+try{
+    $conexao = new PDO("
+    mysql:host=$host;
+    dbname=$banco;
+    charset=utf8", 
+    $usuario,$senha);
 
+$conexao > setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXECEPTION);
 
+}catch(PDOExeception $erro)
 
+{
+    echo "Erro na Conexao" .$erro->getMessage();
+    exit();
+}
 
-
-
-
-
-
-
-
-
-
-?>
